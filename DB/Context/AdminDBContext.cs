62106472a30fe1046a403aef01171ee9837f6bff
1062905638 <1062905638@qq.com>
2025-04-02 17:20:58 +0800

    Admin/User

---
+++ DB/Context/AdminDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.DB.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Context
+{
+    public class AdminDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Admin");
+
+        public required DbSet<User> Users { get; set; }
+    }
+}
